ea7c522c5d5af255b3557d06bf9306b4b50447c7
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-10 14:14:04 +0200

    inti

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace _18.LinqReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] {1, 2, 3, 4, 5, 6 };
+            // int[] array2 = array.Reverse().ToArray();// Reverse() method takes array and reverses it elements!,so we create new reversed array2
+            int[] array2 = new int[] {1, 2, 3, 4, 5, 6 };
+            
+            array2 = array2.Reverse().ToArray();  // we overrite the content of the array 2 in inverted way.
+
+            //Console.WriteLine("Content of array : {0}" , string.Join(" ", array));
+
+            //Console.WriteLine("Content of Reveresed  array is : {0} ", string.Join("," , array2));
+            // Concat()
+
+            //Console.WriteLine(string.Join(" ", array.Concat(array2)));  //We print symtrical Array using the concat Method
+            Console.WriteLine(array.Concat(array2).ToArray()[5]); //concatenate the two arrays together and from the new array take index-> 5
+
+        }
+    }
+}
